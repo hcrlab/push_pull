dcda44582a6d4cc7c23d57d8e10fae4368059b85
Justin Huang <jstn@cs.washington.edu>
2015-04-06 18:55:40 -0700

    Update docs.

--- pr2_pick_manipulation/include/pr2_pick_manipulation/torso_service.h
+++ pr2_pick_manipulation/include/pr2_pick_manipulation/torso_service.h
@@ -1,7 +1,7 @@
 // A service for moving the torso.
 //
 // Sample usage (Python):
-// from pr2_pick_manipulation.srv import Torso
+// from pr2_pick_manipulation.srv import MoveTorso
 // rospy.wait_for_service('torso_service')
 // move_torso = rospy.ServiceProxy('torso_service', Torso)
 // move_torso(0.1) # Move torso to position 10 cm up.
