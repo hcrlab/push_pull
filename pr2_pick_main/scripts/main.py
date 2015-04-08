330d84a16b41cc04eb9909b2e508a3403acc20ed
Justin Huang <jstn@cs.washington.edu>
2015-04-08 16:57:24 -0700

    Fix issue where exceptions thrown by the state machine make main.py impossible to shutdown with Ctrl+C

--- pr2_pick_main/scripts/main.py
+++ pr2_pick_main/scripts/main.py
@@ -3,11 +3,23 @@
 """The main state machine for the picking challenge.
 """
 
+from collections import namedtuple
 import outcomes
 import rospy
 import smach
 import smach_ros
 import states
+
+
+# Bin data structure.
+#   id: string, 'A' - 'L' according to the diagram on the contest website.
+#   visited: boolean, true if the robot has tried to pick an item from this bin
+#     at least once, false otherwise.
+#   succeeded: boolean, true if the robot successfully picked an item from this
+#     bin, false otherwise. succeeded = false either means that there was an
+#     unsuccessful attempt, or it hasn't been tried yet, check visited to
+#     distinguish between these two cases.
+BinData = namedtuple('BinData', ['id', 'visited', 'succeeded'])
 
 
 def main():
@@ -18,8 +30,13 @@ def main():
         outcomes.CHALLENGE_FAILURE
     ])
 
-    # The set of bins we have already attempted to pick from.
-    sm.userdata.last_bin_id = None
+    # The current bin being attempted.
+    sm.userdata.current_bin = None
+
+    # Holds data about the state of each bin.
+    sm.userdata.bin_data = {}
+    for bin_id in 'ABCDEFGHIJKL':
+        sm.userdata.bin_data[bin_id] = BinData(id, False, False)
 
     with sm:
         smach.StateMachine.add(
@@ -45,6 +62,11 @@ def main():
                 outcomes.UPDATE_PLAN_NEXT_OBJECT: states.MoveToBin.name,
                 outcomes.UPDATE_PLAN_NO_MORE_OBJECTS: outcomes.CHALLENGE_SUCCESS,
                 outcomes.UPDATE_PLAN_FAILURE: outcomes.CHALLENGE_FAILURE
+            },
+            remapping={
+                'bin_data': 'bin_data',
+                'output_bin_data': 'bin_data',
+                'next_bin': 'current_bin'
             }
         )
         smach.StateMachine.add(
@@ -53,6 +75,9 @@ def main():
             transitions={
                 outcomes.MOVE_TO_BIN_SUCCESS: states.SenseBin.name,
                 outcomes.MOVE_TO_BIN_FAILURE: outcomes.CHALLENGE_FAILURE
+            },
+            remapping={
+                'bin_id': 'current_bin'
             }
         )
         smach.StateMachine.add(
@@ -62,6 +87,9 @@ def main():
                 outcomes.SENSE_BIN_SUCCESS: states.Grasp.name,
                 outcomes.SENSE_BIN_NO_OBJECTS: states.UpdatePlan.name,
                 outcomes.SENSE_BIN_FAILURE: outcomes.CHALLENGE_FAILURE
+            },
+            remapping={
+                'bin_id': 'current_bin'
             }
         )
         smach.StateMachine.add(
@@ -72,6 +100,9 @@ def main():
                 outcomes.GRASP_FAILURE: (
                     outcomes.CHALLENGE_FAILURE
                 )
+            },
+            remapping={
+                'bin_id': 'current_bin'
             }
         )
         smach.StateMachine.add(
@@ -80,6 +111,9 @@ def main():
             transitions={
                 outcomes.EXTRACT_ITEM_SUCCESS: states.DropOffItem.name,
                 outcomes.EXTRACT_ITEM_FAILURE: states.UpdatePlan.name
+            },
+            remapping={
+                'bin_id': 'current_bin'
             }
         )
         smach.StateMachine.add(
@@ -88,16 +122,25 @@ def main():
             transitions={
                 outcomes.DROP_OFF_ITEM_SUCCESS: states.UpdatePlan.name,
                 outcomes.DROP_OFF_ITEM_FAILURE: states.UpdatePlan.name
+            },
+            remapping={
+                'bin_id': 'current_bin',
+                'bin_data': 'bin_data'
             }
         )
 
-    sis = smach_ros.IntrospectionServer('state_machine_introspection_server',
-                                        sm,
-                                        '/')
-    sis.start()
-    outcome = sm.execute()
-    rospy.spin()
-    sis.stop()
+        try:
+            sis = smach_ros.IntrospectionServer('state_machine_introspection_server',
+                                                sm,
+                                                '/')
+            sis.start()
+            outcome = sm.execute()
+        except:
+            sis.stop()
+            rospy.signal_shutdown('Exception in the state machine.')
+
+        rospy.spin()
+        sis.stop()
 
 
 if __name__ == '__main__':
--- pr2_pick_main/scripts/states/DropOffItem.py
+++ pr2_pick_main/scripts/states/DropOffItem.py
@@ -14,9 +14,11 @@ class DropOffItem(smach.State):
                 outcomes.DROP_OFF_ITEM_SUCCESS,
                 outcomes.DROP_OFF_ITEM_FAILURE
             ],
-            input_keys=['bin_id']
+            input_keys=['bin_id'],
+            output_keys=['bin_data']
         )
 
     def execute(self, userdata):
         rospy.loginfo('Dropping off item from bin {}'.format(userdata.bin_id))
+        userdata.bin_data[bin_id].succeeded = True
         return outcomes.DROP_OFF_ITEM_SUCCESS
