b0632b96ea62e425d9ca345eb638a83a39f06f71
Sarah Elliott <sksellio@enterprise.(none)>
2015-04-08 19:02:19 -0700

    Rough attempt for Grasp and ExtractItem states

--- pr2_pick_main/scripts/states/ExtractItem.py
+++ pr2_pick_main/scripts/states/ExtractItem.py
@@ -1,6 +1,8 @@
 import outcomes
 import rospy
 import smach
+import moveit_commander
+import geometry_msgs.msg
 
 
 class ExtractItem(smach.State):
@@ -20,4 +22,84 @@ class ExtractItem(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('Extracting item in bin {}'.format(userdata.bin_id))
+
+        robot = moveit_commander.RobotCommander()
+
+        group = moveit_commander.MoveGroupCommander("right_arm")
+
+        # Poses for each bin
+
+        bin_dict = {}
+        bin_dict["A"]["lift"] = geometry_msgs.msg.Pose()
+        bin_dict["A"]["lift"].orientation.w = 1;
+        bin_dict["A"]["lift"].position.x = 0.2;
+        bin_dict["A"]["lift"].position.y = 0.0;
+        bin_dict["A"]["lift"].position.z = 1.95;
+
+        bin_dict["A"]["extract"] = geometry_msgs.msg.Pose()
+        bin_dict["A"]["extract"].orientation.w = 1;
+        bin_dict["A"]["extract"].position.x = 0.35;
+        bin_dict["A"]["extract"].position.y = 0.0;
+        bin_dict["A"]["extract"].position.z = 1.95;
+
+        bin_dict["D"]["lift"] = geometry_msgs.msg.Pose()
+        bin_dict["D"]["lift"].orientation.w = 1;
+        bin_dict["D"]["lift"].position.x = 0.2;
+        bin_dict["D"]["lift"].position.y = 0.0;
+        bin_dict["D"]["lift"].position.z = 1.65;
+
+        bin_dict["D"]["extract"] = geometry_msgs.msg.Pose()
+        bin_dict["D"]["extract"].orientation.w = 1;
+        bin_dict["D"]["extract"].position.x = 0.35;
+        bin_dict["D"]["extract"].position.y = 0.0;
+        bin_dict["D"]["extract"].position.z = 1.65;
+
+        bin_dict["G"]["lift"] = geometry_msgs.msg.Pose()
+        bin_dict["G"]["lift"].orientation.w = 1;
+        bin_dict["G"]["lift"].position.x = 0.2;
+        bin_dict["G"]["lift"].position.y = 0.0;
+        bin_dict["G"]["lift"].position.z = 1.35;
+
+        bin_dict["G"]["extract"] = geometry_msgs.msg.Pose()
+        bin_dict["G"]["extract"].orientation.w = 1;
+        bin_dict["G"]["extract"].position.x = 0.35;
+        bin_dict["G"]["extract"].position.y = 0.0;
+        bin_dict["G"]["extract"].position.z = 1.35;
+
+        bin_dict["J"]["lift"] = geometry_msgs.msg.Pose()
+        bin_dict["J"]["lift"].orientation.w = 1;
+        bin_dict["J"]["lift"].position.x = 0.2;
+        bin_dict["J"]["lift"].position.y = 0.0;
+        bin_dict["J"]["lift"].position.z = 1.05;
+
+        bin_dict["J"]["extract"] = geometry_msgs.msg.Pose()
+        bin_dict["J"]["extract"].orientation.w = 1;
+        bin_dict["J"]["extract"].position.x = 0.35;
+        bin_dict["J"]["extract"].position.y = 0.0;
+        bin_dict["J"]["extract"].position.z = 1.05;
+
+        bin_dict["B"] = bin_dict["C"] = bin_dict["A"]
+
+        bin_dict["E"] = bin_dict["F"] = bin_dict["D"]
+
+        bin_dict["H"] = bin_dict["I"] = bin_dict["G"]
+
+        bin_dict["K"] = bin_dict["L"] = bin_dict["J"]
+
+
+        # Lift item to clear bin lip
+            
+        group.set_pose_target(bin_dict[userdata.bin_id]["lift"])
+        plan = group.plan()
+        group.go(wait=True)
+
+        # Pull item out of bin
+
+        group.set_pose_target(bin_dict[userdata.bin_id]["extract"])
+        plan = group.plan()
+        group.go(wait=True)
+
+
+
+
         return outcomes.EXTRACT_ITEM_SUCCESS
--- pr2_pick_main/scripts/states/Grasp.py
+++ pr2_pick_main/scripts/states/Grasp.py
@@ -1,6 +1,8 @@
 import outcomes
 import rospy
 import smach
+import moveit_commander
+import geometry_msgs.msg
 
 
 class Grasp(smach.State):
@@ -20,4 +22,94 @@ class Grasp(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('Grasping item in bin {}'.format(userdata.bin_id))
+
+        robot = moveit_commander.RobotCommander()
+
+        group = moveit_commander.MoveGroupCommander("right_arm")
+
+        rospy.wait_for_service('gripper_service')
+        self._set_grippers = rospy.ServiceProxy('gripper_service', SetGrippers)
+
+        # Poses for each bin
+
+        bin_dict = {}
+        bin_dict["A"]["pre-grasp"] = geometry_msgs.msg.Pose()
+        bin_dict["A"]["pre-grasp"].orientation.w = 1;
+        bin_dict["A"]["pre-grasp"].position.x = 0.2;
+        bin_dict["A"]["pre-grasp"].position.y = 0.0;
+        bin_dict["A"]["pre-grasp"].position.z = 1.9;
+
+        bin_dict["A"]["grasp"] = geometry_msgs.msg.Pose()
+        bin_dict["A"]["grasp"].orientation.w = 1;
+        bin_dict["A"]["grasp"].position.x = 0.35;
+        bin_dict["A"]["grasp"].position.y = 0.0;
+        bin_dict["A"]["grasp"].position.z = 1.9;
+
+        bin_dict["D"]["pre-grasp"] = geometry_msgs.msg.Pose()
+        bin_dict["D"]["pre-grasp"].orientation.w = 1;
+        bin_dict["D"]["pre-grasp"].position.x = 0.2;
+        bin_dict["D"]["pre-grasp"].position.y = 0.0;
+        bin_dict["D"]["pre-grasp"].position.z = 1.6;
+
+        bin_dict["D"]["grasp"] = geometry_msgs.msg.Pose()
+        bin_dict["D"]["grasp"].orientation.w = 1;
+        bin_dict["D"]["grasp"].position.x = 0.35;
+        bin_dict["D"]["grasp"].position.y = 0.0;
+        bin_dict["D"]["grasp"].position.z = 1.6;
+
+        bin_dict["G"]["pre-grasp"] = geometry_msgs.msg.Pose()
+        bin_dict["G"]["pre-grasp"].orientation.w = 1;
+        bin_dict["G"]["pre-grasp"].position.x = 0.2;
+        bin_dict["G"]["pre-grasp"].position.y = 0.0;
+        bin_dict["G"]["pre-grasp"].position.z = 1.3;
+
+        bin_dict["G"]["grasp"] = geometry_msgs.msg.Pose()
+        bin_dict["G"]["grasp"].orientation.w = 1;
+        bin_dict["G"]["grasp"].position.x = 0.35;
+        bin_dict["G"]["grasp"].position.y = 0.0;
+        bin_dict["G"]["grasp"].position.z = 1.3;
+
+        bin_dict["J"]["pre-grasp"] = geometry_msgs.msg.Pose()
+        bin_dict["J"]["pre-grasp"].orientation.w = 1;
+        bin_dict["J"]["pre-grasp"].position.x = 0.2;
+        bin_dict["J"]["pre-grasp"].position.y = 0.0;
+        bin_dict["J"]["pre-grasp"].position.z = 1.0;
+
+        bin_dict["J"]["grasp"] = geometry_msgs.msg.Pose()
+        bin_dict["J"]["grasp"].orientation.w = 1;
+        bin_dict["J"]["grasp"].position.x = 0.35;
+        bin_dict["J"]["grasp"].position.y = 0.0;
+        bin_dict["J"]["grasp"].position.z = 1.0;
+
+        bin_dict["B"] = bin_dict["C"] = bin_dict["A"]
+
+        bin_dict["E"] = bin_dict["F"] = bin_dict["D"]
+
+        bin_dict["H"] = bin_dict["I"] = bin_dict["G"]
+
+        bin_dict["K"] = bin_dict["L"] = bin_dict["J"]
+
+
+        # Pose in front of bin
+            
+        group.set_pose_target(bin_dict[userdata.bin_id]["pre-grasp"])
+        plan = group.plan()
+        group.go(wait=True)
+
+        # Open Hand
+
+        grippers_open = self._set_grippers(True, True)
+
+        # Move into bin
+
+        group.set_pose_target(bin_dict[userdata.bin_id]["grasp"])
+        plan = group.plan()
+        group.go(wait=True)
+
+        # Close hand
+
+        grippers_open = self._set_grippers(False, False)
+
+
+
         return outcomes.GRASP_SUCCESS
