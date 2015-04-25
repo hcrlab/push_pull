da12ee6d7219daab63034c74b3555289fa55a22f
Justin Huang <jstn@cs.washington.edu>
2015-04-24 20:02:27 -0700

    Add MoveArmIk implementation.

--- pr2_pick_manipulation/src/moveit_service_node.py
+++ pr2_pick_manipulation/src/moveit_service_node.py
@@ -16,9 +16,10 @@ class ArmMover:
         self._default_planning_time = 8
         self._default_position_tolerance = 0.0001
         self._default_orientation_tolerance = 0.001
-        self._group = moveit_commander.MoveGroupCommander("right_arm") 
-        #self._group_left = moveit_commander.MoveGroupCommander("left_arm")
+        self._left_group = moveit_commander.MoveGroupCommander("left_arm")
+        self._right_group = moveit_commander.MoveGroupCommander("right_arm") 
         self._s = rospy.Service('moveit_service', MoveArm, self.move_arm)
+        self._ik_service = rospy.Service('move_arm_ik', MoveArmIk, self.move_arm_ik)
 
     def move_arm(self, req):
 
@@ -32,28 +33,36 @@ class ArmMover:
                 # TODO(jstn): maybe instead of creating a new commander every service call,
                 # we can just create a commander for each move_group in __init__, and then
                 # choose the appropriate one here?
-                self._group = moveit_commander.MoveGroupCommander(req.group)
+                group = None
+                if req.group == 'right_arm':
+                    group = self._right_group
+                elif req.group == 'left_arm':
+                    group = self._left_group
+                else:
+                    rospy.logwarn('Calling moveit_service with untested group: {}'.format(req.group))
+                    group = moveit_commander.MoveGroupCommander(req.group)
+                #self._group = moveit_commander.MoveGroupCommander(req.group)
 
                 if req.position_tolerance > 0:
-                    self._group.set_goal_position_tolerance(req.position_tolerance)
+                    group.set_goal_position_tolerance(req.position_tolerance)
                 else:
-                    self._group.set_goal_position_tolerance(self._default_position_tolerance)
+                    group.set_goal_position_tolerance(self._default_position_tolerance)
 
                 if req.orientation_tolerance > 0:
-                    self._group.set_goal_orientation_tolerance(req.orientation_tolerance)
+                    group.set_goal_orientation_tolerance(req.orientation_tolerance)
                 else:
-                    self._group.set_goal_orientation_tolerance(self._default_orientation_tolerance)
+                    group.set_goal_orientation_tolerance(self._default_orientation_tolerance)
 
-                self._group.set_pose_target(req.goal)
+                group.set_pose_target(req.goal)
 
                 if req.planning_time > 0:
-                    self._group.set_planning_time(req.planning_time)
+                    group.set_planning_time(req.planning_time)
                 else:
-                    self._group.set_planning_time(self._default_planning_time)
+                    group.set_planning_time(self._default_planning_time)
 
-                plan = self._group.plan()
+                plan = group.plan()
             
-                success = self._group.go(wait=True)
+                success = group.go(wait=True)
 
                 print "Plan: " + str(len(plan.joint_trajectory.points))
                 print "Success: " + str(success)
@@ -66,6 +75,24 @@ class ArmMover:
                     return MoveArmResponse(False)
             except RuntimeError:
                 rospy.sleep(3.0)
+
+    def move_arm_ik(self, request):
+        group = None
+        if request.arm == MoveArmIkRequest.LEFT_ARM:
+            group = self._left_group
+        else:
+            group = self._right_group
+        
+        # TODO(jstn): TEST ################################
+        group = moveit_commander.MoveGroupCommander('right_arm')
+        request = MoveArmIkRequest()
+
+        waypoints = [group.get_current_pose(), request.goal]
+        rospy.loginfo(waypoints)
+        group.compute_cartesian_path(waypoints, 0.01, 0, avoid_collisions=False)
+
+        success = group.go(wait=True)
+        return MoveArmIkResponse(success)
 
 
 if __name__ == "__main__":
