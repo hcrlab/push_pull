2430f69f4da58a0c3b74b8a1a9de7fbde5d5c78d
Sarah Elliott <sksellio@enterprise.(none)>
2015-04-25 21:23:52 -0700

    Untested service to return number of points within bounding box

--- pr2_pick_perception/scripts/pcl_utilities.py
+++ pr2_pick_perception/scripts/pcl_utilities.py
@@ -6,7 +6,9 @@ import rospy
 from sensor_msgs.msg import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
 from std_msgs.msg import Header
+import tf
 
+from pr2_pick_perception.srv import BoxPoints, BoxPointsResponse 
 from pr2_pick_perception.srv import FindCentroid, FindCentroidResponse
 
 class PCLUtilities(object):
@@ -17,6 +19,12 @@ class PCLUtilities(object):
             'perception/find_centroid',
             FindCentroid,
             self.find_centroid
+        )
+
+        self._points_in_box_service = rospy.Service(
+            'perception/points_in_box',
+            BoxPoints,
+            self.find_points_in_box
         )
 
     def find_centroid(self, request):
@@ -50,6 +58,40 @@ class PCLUtilities(object):
             )
         )
         return FindCentroidResponse(centroid=centroid)
+
+    def find_points_in_box(self, request):
+        ''' Returns number of points within bounding box specified by 
+            request. '''        
+        points = pc2.read_points(
+            request.cluster.pointcloud,
+            field_names=['x', 'y', 'z'],
+            skip_nans=True,
+        )
+
+        num_points = 0
+        tf_listener = tf.TransformListener()
+        for x, y, z in points:
+
+            # Transform point into frame of bounding box
+            point = PointStamped(
+                point=Point(x=x, y=y, z=z),
+                header=Header(
+                    frame_id=request.cluster.header.frame_id,
+                    stamp=rospy.Time.now(),
+                )
+            )
+
+            transformed_point = tf_listener.transformPose('r_wrist_roll_link',
+                                                                point)
+            if (transformed_point.point.x => request.min_x and
+                transformed_point.point.x <= request.max_x and
+                transformed_point.point.y => request.min_y and
+                transformed_point.point.y <= request.max_y and
+                transformed_point.point.z => request.min_z and
+                transformed_point.point.z <= request.max_z):
+                num_points += 1
+
+        return BoxPointsResponse(num_points=num_points)
 
 
 def main():
