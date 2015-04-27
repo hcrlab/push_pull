4e9a35705de045d302f387808fea39e14bad4b56
Justin Huang <jstn@cs.washington.edu>
2015-04-27 11:18:39 -0700

    Add cartesian arm movement service.

--- pr2_pick_manipulation/src/moveit_service_node.py
+++ pr2_pick_manipulation/src/moveit_service_node.py
@@ -6,9 +6,10 @@ from pr2_pick_manipulation.srv import *
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
+import tf
+
 
 class ArmMover:
-
     def __init__(self):
         rospy.init_node('moveit_service_node')
 
@@ -17,41 +18,43 @@ class ArmMover:
         self._default_position_tolerance = 0.0001
         self._default_orientation_tolerance = 0.001
         self._left_group = moveit_commander.MoveGroupCommander("left_arm")
-        self._right_group = moveit_commander.MoveGroupCommander("right_arm") 
+        self._right_group = moveit_commander.MoveGroupCommander("right_arm")
         self._s = rospy.Service('moveit_service', MoveArm, self.move_arm)
-        self._ik_service = rospy.Service('move_arm_ik', MoveArmIk, self.move_arm_ik)
+        self._ik_service = rospy.Service('move_arm_ik', MoveArmIk,
+                                         self.move_arm_ik)
+        self._tf = tf.TransformListener()
 
     def move_arm(self, req):
-
         plan = None
         success = False
 
         rospy.loginfo("Requested to move: " + req.group)
 
         while plan is None:
-            try: 
-                # TODO(jstn): maybe instead of creating a new commander every service call,
-                # we can just create a commander for each move_group in __init__, and then
-                # choose the appropriate one here?
+            try:
                 group = None
                 if req.group == 'right_arm':
                     group = self._right_group
                 elif req.group == 'left_arm':
                     group = self._left_group
                 else:
-                    rospy.logwarn('Calling moveit_service with untested group: {}'.format(req.group))
+                    rospy.logwarn(
+                        'Calling moveit_service with untested group: {}'.format(
+                            req.group))
                     group = moveit_commander.MoveGroupCommander(req.group)
-                #self._group = moveit_commander.MoveGroupCommander(req.group)
 
                 if req.position_tolerance > 0:
                     group.set_goal_position_tolerance(req.position_tolerance)
                 else:
-                    group.set_goal_position_tolerance(self._default_position_tolerance)
+                    group.set_goal_position_tolerance(
+                        self._default_position_tolerance)
 
                 if req.orientation_tolerance > 0:
-                    group.set_goal_orientation_tolerance(req.orientation_tolerance)
+                    group.set_goal_orientation_tolerance(
+                        req.orientation_tolerance)
                 else:
-                    group.set_goal_orientation_tolerance(self._default_orientation_tolerance)
+                    group.set_goal_orientation_tolerance(
+                        self._default_orientation_tolerance)
 
                 group.set_pose_target(req.goal)
 
@@ -61,12 +64,12 @@ class ArmMover:
                     group.set_planning_time(self._default_planning_time)
 
                 plan = group.plan()
-            
+
                 success = group.go(wait=True)
 
                 print "Plan: " + str(len(plan.joint_trajectory.points))
                 print "Success: " + str(success)
-                   
+
                 if plan.joint_trajectory.points and success:
                     print "Moved arm successfully!"
                     return MoveArmResponse(True)
@@ -77,26 +80,36 @@ class ArmMover:
                 rospy.sleep(3.0)
 
     def move_arm_ik(self, request):
+        """Moves the arm using MoveIt's Cartesian path planner.
+
+        This moves the arm in a straight line to the goal pose. Returns True if
+        the execution succeeded, False otherwise (e.g., the arm gets stuck).
+        """
         group = None
         if request.arm == MoveArmIkRequest.LEFT_ARM:
             group = self._left_group
         else:
             group = self._right_group
-        
-        # TODO(jstn): TEST ################################
-        group = moveit_commander.MoveGroupCommander('right_arm')
-        request = MoveArmIkRequest()
 
-        waypoints = [group.get_current_pose(), request.goal]
-        rospy.loginfo(waypoints)
-        group.compute_cartesian_path(waypoints, 0.01, 0, avoid_collisions=False)
+        request.goal.header.stamp = rospy.Time(0)
+        pose_in_planning_frame = None
+        try:
+            pose_in_planning_frame = self._tf.transformPose(
+                group.get_planning_frame(), request.goal)
+        except:
+            rospy.logerr('[MoveArmIk]: No TF from {} to {}'.format(
+                request.goal.header.frame_id, group.get_planning_frame()))
+            return MoveArmIkResponse(False)
 
-        success = group.go(wait=True)
+        waypoints = [pose_in_planning_frame.pose]
+        trajectory, _ = group.compute_cartesian_path(waypoints, 0.01, 0,
+                                                     avoid_collisions=False)
+
+        group.clear_pose_targets()
+        success = group.execute(trajectory)
         return MoveArmIkResponse(success)
 
 
 if __name__ == "__main__":
-
-    arm  = ArmMover()
-
+    arm = ArmMover()
     rospy.spin()
