f3b52d1400d77f881485f994627a66b56284c272
bgardon <benbwgardon@gmail.com>
2015-04-28 11:57:11 -0700

    Some progrss on test_update_plan

--- pr2_pick_main/scripts/test_update_plan.py
+++ pr2_pick_main/scripts/test_update_plan.py
@@ -1,36 +1,52 @@
 #! /usr/bin/env python
 
 from states.UpdatePlan import UpdatePlan
-
-# To execute GraspTool state while developing it
+import unittest
+import rospy
+import ros
+from std_msgs.msg import String
+import mock
 
 class MockUserdata(object):
     ''' Stand-in for smach's userdata object '''
 
 class TestUpdatePlan(unittest.TestCase):
-    def setUp(self):
-    	print "candy"
+    def side_effect(self, name, return_value=True):
+        def wrapped(*args, **kwargs):
+            rospy.loginfo('Calling {}{}'.format(name, args))
+            return return_value
+        return wrapped
 
-	    tts = rospy.Publisher('/festival_tts', String)
+    def setUp(self):
+        print 'candy'
+        tts = rospy.Publisher('/festival_tts', String)
         tts.publish = mock.Mock(side_effect=self.side_effect('tts'))
 
         get_items = mock.Mock(return_value={'book', 'box'})
-        set_items = mock.Mock(return_value=true)
+        set_items = mock.Mock(return_value=True)
         get_target_items = mock.Mock(return_value={'book'})
-        self.kwargs = {
+
+        # get_items = mock.Mock(side_effect=self.side_effect('get_items'))
+        # set_items = mock.Mock(side_effect=self.side_effect('set_items'))
+        # get_target_items = mock.Mock(side_effect=self.side_effect('get_target_items'))
+
+        kwargs = {
             'tts': tts,
             'get_items': get_items,
             'set_items': set_items,
             'get_target_items': get_target_items
         }
 
-	    self.state = UpdatePlan(self.kwargs)
-	    self.mock_user_data = MockUserdata()
-	    self.mock_user_data.debug = True
+        self.kwargs = kwargs
 
-	def test_one(self):
-		result = self.state.execute(self.mock_user_data)
-	    print result
+        self.state = UpdatePlan()
+        self.mock_user_data = MockUserdata()
+        self.mock_user_data.debug = True
+
+    def test_one(self):
+        self.assertEqual(1, 1)
+        result = self.state.execute(self.mock_user_data)
+        print result
 
 
 if __name__ == '__main__':
