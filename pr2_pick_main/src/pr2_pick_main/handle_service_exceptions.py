58f0beb2f95c499f9588a0b5a2766a5fcd5e8207
Justin Huang <jstn@cs.washington.edu>
2015-05-11 16:04:03 -0700

    Add handle_service_exceptions to pr2_pick_main library.

---
+++ pr2_pick_main/src/pr2_pick_main/handle_service_exceptions.py
@@ -0,0 +1,18 @@
+def handle_service_exceptions(outcome):
+    """A decorator for smach states to handle all service exceptions.
+    Makes the state return the given outcome on failure.
+
+    Usage:
+    @handle_service_exceptions(outcomes.FIND_SHELF_FAILURE)
+    def execute(self, userdata):
+        ...
+    """
+    def with_outcome(execute):
+        def wrapped_execute(self, userdata):
+            try:
+                return execute(self, userdata)
+            except rospy.ServiceException as e:
+                rospy.logerr(e)
+                return outcome
+        return wrapped_execute
+    return with_outcome
