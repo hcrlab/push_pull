66a9058617b59e7db3986c724dea269cdbbe73a6
Justin Huang <jstn@cs.washington.edu>
2015-05-11 22:20:03 -0700

    Fix wrong point cloud data for count_points_in_box.

--- pr2_pick_perception/src/count_points_in_box_service.cpp
+++ pr2_pick_perception/src/count_points_in_box_service.cpp
@@ -28,6 +28,7 @@ CountPointsInBoxService::CountPointsInBoxService(const std::string& name,
 
 bool CountPointsInBoxService::Callback(CountPointsInBox::Request& request,
                                        CountPointsInBox::Response& response) {
+  // Get point cloud and remove NaNs.
   ROS_INFO("Waiting for point cloud.");
   boost::shared_ptr<const sensor_msgs::PointCloud2> ros_cloud =
       ros::topic::waitForMessage<sensor_msgs::PointCloud2>(pc_topic_);
@@ -37,9 +38,22 @@ bool CountPointsInBoxService::Callback(CountPointsInBox::Request& request,
   pcl::PointCloud<pcl::PointXYZRGB> pcl_cloud;
   std::vector<int> index;
   pcl::removeNaNFromPointCloud(pcl_cloud_unfiltered, pcl_cloud, index);
+
+  // Transform point cloud to request frame.
   pcl::PointCloud<pcl::PointXYZRGB> request_cloud;
-  pcl_ros::transformPointCloud(request.frame_id.data, pcl_cloud, request_cloud,
-                               tf_listener_);
+  std::string error_msg;
+  tf::StampedTransform cloud_to_request;
+  if (!tf_listener_.canTransform("torso_lift_link", ros_cloud->header.frame_id,
+                                 ros::Time(0), &error_msg)) {
+    ROS_ERROR("%s", error_msg.c_str());
+    return false;
+  } else {
+    ROS_INFO("Transforming from %s to %s", ros_cloud->header.frame_id.c_str(),
+             request.frame_id.c_str());
+    tf_listener_.lookupTransform(request.frame_id, ros_cloud->header.frame_id,
+                                 ros::Time(0), cloud_to_request);
+  }
+  pcl_ros::transformPointCloud(pcl_cloud, request_cloud, cloud_to_request);
 
   double box_back = request.center.x + request.dimensions.x / 2.0;
   double box_front = request.center.x - request.dimensions.x / 2.0;
@@ -47,8 +61,9 @@ bool CountPointsInBoxService::Callback(CountPointsInBox::Request& request,
   double box_right = request.center.y - request.dimensions.y / 2.0;
   double box_top = request.center.z + request.dimensions.z / 2.0;
   double box_bottom = request.center.z - request.dimensions.z / 2.0;
-  for (size_t i = 0; i < pcl_cloud.size(); ++i) {
-    const pcl::PointXYZRGB& point = pcl_cloud[i];
+
+  for (size_t i = 0; i < request_cloud.size(); ++i) {
+    const pcl::PointXYZRGB& point = request_cloud[i];
     if (point.x < box_back && point.x >= box_front && point.y < box_left &&
         point.y >= box_right && point.z < box_top && point.z >= box_bottom) {
       response.num_points++;
