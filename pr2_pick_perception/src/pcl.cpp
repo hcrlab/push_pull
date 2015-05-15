c0fb609c17933d922dac22776cdcf89dc8dfa47e
Justin Huang <jstn@cs.washington.edu>
2015-05-15 16:13:39 -0700

    Add color histogram based on RGB values jointly.

--- pr2_pick_perception/src/pcl.cpp
+++ pr2_pick_perception/src/pcl.cpp
@@ -162,8 +162,9 @@ void GetPlanarBoundingBox(const PointCloud<PointXYZRGB>& cloud,
   dimensions->z = z_length;
 }
 
-void ComputeColorHistogram(const PointCloud<PointXYZRGB>& cloud,
-                           const int num_bins, std::vector<int>* histogram) {
+void ComputeColorHistogramSeparate(const PointCloud<PointXYZRGB>& cloud,
+                                   const int num_bins,
+                                   std::vector<int>* histogram) {
   double bin_size = 255.0 / num_bins;
   histogram->clear();
   histogram->resize(3 * num_bins);
@@ -186,6 +187,32 @@ void ComputeColorHistogram(const PointCloud<PointXYZRGB>& cloud,
     (*histogram)[red_bin] += 1;
     (*histogram)[num_bins + green_bin] += 1;
     (*histogram)[num_bins + num_bins + blue_bin] += 1;
+  }
+}
+
+void ComputeColorHistogram(const PointCloud<PointXYZRGB>& cloud,
+                           const int num_bins, std::vector<int>* histogram) {
+  double bin_size = 255.0 / num_bins;
+  histogram->clear();
+  histogram->resize(num_bins * num_bins * num_bins);
+  for (size_t i = 0; i < histogram->size(); ++i) {
+    (*histogram)[i] = 0;
+  }
+
+  for (size_t i = 0; i < cloud.size(); ++i) {
+    const PointXYZRGB& point = cloud[i];
+    uint8_t red = point.r;
+    uint8_t green = point.g;
+    uint8_t blue = point.b;
+
+    int red_bin =
+        std::min(static_cast<int>(floor(red / bin_size)), num_bins - 1);
+    int green_bin =
+        std::min(static_cast<int>(floor(green / bin_size)), num_bins - 1);
+    int blue_bin =
+        std::min(static_cast<int>(floor(blue / bin_size)), num_bins - 1);
+    int index = red_bin * num_bins * num_bins + green_bin * num_bins + blue_bin;
+    (*histogram)[index] += 1;
   }
 }
 
