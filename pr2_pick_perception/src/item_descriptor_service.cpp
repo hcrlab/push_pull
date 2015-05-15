a160adbd71403480a92814edc1fd7040010bc604
Justin Huang <jstn@cs.washington.edu>
2015-05-15 16:15:03 -0700

    Revert changes to item descriptor while alternative color histogram is in testing.

--- pr2_pick_perception/src/item_descriptor_service.cpp
+++ pr2_pick_perception/src/item_descriptor_service.cpp
@@ -23,7 +23,7 @@ ItemDescriptorService::ItemDescriptorService(const std::string& name)
       server_(
           nh_.advertiseService(name, &ItemDescriptorService::Callback, this)),
       num_bins_(4) {
-  ros::param::param<double>("color_histogram/num_bins", num_bins_, 3);
+  ros::param::param<double>("color_histogram/num_bins", num_bins_, 4);
 }
 
 bool ItemDescriptorService::Callback(GetItemDescriptor::Request& request,
@@ -37,7 +37,7 @@ bool ItemDescriptorService::Callback(GetItemDescriptor::Request& request,
 
   // Get color histogram.
   std::vector<int> histogram;
-  ComputeColorHistogram(pcl_cloud, num_bins_, &histogram);
+  ComputeColorHistogramSeparate(pcl_cloud, num_bins_, &histogram);
   ColorHistogram histogram_msg;
   histogram_msg.num_bins = num_bins_;
   histogram_msg.histogram = histogram;
