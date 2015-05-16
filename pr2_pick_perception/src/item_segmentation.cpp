91f91534b4002561bf73ce56799bba0cc39f20cb
Justin Huang <jstn@cs.washington.edu>
2015-05-15 21:25:56 -0700

    Add item segmentation service.

--- pr2_pick_perception/src/item_segmentation.cpp
+++ pr2_pick_perception/src/item_segmentation.cpp
@@ -20,61 +20,65 @@ using pcl::PointXYZRGB;
 namespace pr2_pick_perception {
 ItemSegmentationService::ItemSegmentationService(const std::string& name)
     : name_(name),
-      nh_(name),
+      nh_(),
       nh_local_("~"),
+      server_(
+          nh_.advertiseService(name, &ItemSegmentationService::Callback, this)),
       min_cluster_points_(150),
-      max_cluster_points_(100000),
-      server_(nh_.advertiseService(name, &ItemSegmentationService::Callback,
-                                   this)) {
+      max_cluster_points_(100000) {
   nh_local_.param("min_cluster_points", min_cluster_points_, 150);
   nh_local_.param("max_cluster_points", max_cluster_points_, 100000);
 }
 
-bool ItemSegmentationService::Callback(SegmentItems::Request& request,
-                                       SegmentItems::Response& response) {
-  sensor_msgs::PointCloud2& cell_pc_ros = request.cloud;
-  PointCloud<PointXYZRGB>::Ptr cell_pc(new PointCloud<PointXYZRGB>());
-  pcl::fromROSMsg(*cell_pc, cell_pc_ros);
-
-  std::vector<PointCloud<PointXYZRGB>::Ptr> clusters;
-
+void ItemSegmentationService::ClusterWithEuclidean(
+    const pcl::PointCloud<pcl::PointXYZRGB>& cloud,
+    std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr>* clusters) {
   pcl::search::KdTree<PointXYZRGB>::Ptr tree(
       new pcl::search::KdTree<PointXYZRGB>);
-  tree->setInputCloud(cell_pc);
+  tree->setInputCloud(cloud.makeShared());
   std::vector<pcl::PointIndices> clustersInd;
   pcl::EuclideanClusterExtraction<PointXYZRGB> ec;
   ec.setClusterTolerance(0.01);
   ec.setMinClusterSize(min_cluster_points_);
   ec.setMaxClusterSize(max_cluster_points_);
   ec.setSearchMethod(tree);
-  ec.setInputCloud(cell_pc);
+  ec.setInputCloud(cloud.makeShared());
   ec.extract(clustersInd);
 
-  for (int i = 0; i < clustersInd.size(); ++i) {
+  for (size_t i = 0; i < clustersInd.size(); ++i) {
     PointCloud<PointXYZRGB>::Ptr cluster(new PointCloud<PointXYZRGB>);
     PointXYZRGB point;
-    for (int j = 0; j < clustersInd[i].indices.size(); j++) {
+    for (size_t j = 0; j < clustersInd[i].indices.size(); j++) {
       int index = clustersInd[i].indices[j];
-      point.x = cell_pc->points[index].x;
-      point.y = cell_pc->points[index].y;
-      point.z = cell_pc->points[index].z;
-      point.r = cell_pc->points[index].r;
-      point.g = cell_pc->points[index].g;
-      point.b = cell_pc->points[index].b;
-      cluster->points.push_back(point);
+      const PointXYZRGB& point = cloud[index];
+      cluster->push_back(point);
     }
-    cluster->width = cluster->points.size();
+    cluster->width = cluster->size();
     cluster->height = 1;
     cluster->is_dense = true;
-    clusters.push_back(cluster);
+    clusters->push_back(cluster);
   }
+}
 
-  // copy the clusters to the object ObjectList
-  pr2_pick_perception::ClusterList clusterlist;
-  for (int i = 0; i < clusters.size(); i++) {
+bool ItemSegmentationService::Callback(SegmentItems::Request& request,
+                                       SegmentItems::Response& response) {
+  // Get input cloud.
+  sensor_msgs::PointCloud2& cell_pc_ros = request.cloud;
+  std::string cloud_frame_id = request.cloud.header.frame_id;
+  ros::Time cloud_stamp = request.cloud.header.stamp;
+  PointCloud<PointXYZRGB>::Ptr cell_pc(new PointCloud<PointXYZRGB>());
+  pcl::fromROSMsg(cell_pc_ros, *cell_pc);
+
+  // Do clustering.
+  std::vector<PointCloud<PointXYZRGB>::Ptr> clusters;
+  ClusterWithEuclidean(*cell_pc, &clusters);
+
+  // Copy the clusters back to the response.
+  pr2_pick_perception::ClusterList& clusterlist = response.clusters;
+  for (size_t i = 0; i < clusters.size(); i++) {
     pr2_pick_perception::Cluster cluster;
-    cluster.header.frame_id = bin_frame_id_;
-    cluster.header.stamp = pc_timestamp_;
+    cluster.header.frame_id = cloud_frame_id;
+    cluster.header.stamp = cloud_stamp;
     std::stringstream ss;
     ss << "cluster_" << i;
     cluster.id = ss.str();
@@ -82,11 +86,12 @@ bool ItemSegmentationService::Callback(SegmentItems::Request& request,
 
     clusterlist.clusters.push_back(cluster);
   }
+
   if (clusters.size() == 0) {
     ROS_INFO("No clusters found. Returning whole cropped PC");
     pr2_pick_perception::Cluster cluster;
-    cluster.header.frame_id = bin_frame_id_;
-    cluster.header.stamp = pc_timestamp_;
+    cluster.header.frame_id = cloud_frame_id;
+    cluster.header.stamp = cloud_stamp;
     std::stringstream ss;
     ss << "cluster_0";
     cluster.id = ss.str();
@@ -94,6 +99,14 @@ bool ItemSegmentationService::Callback(SegmentItems::Request& request,
 
     clusterlist.clusters.push_back(cluster);
   }
-  response.locations = clusterlist;
+
+  return true;
 }
-};
+}  // namespace pr2_pick_perception
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "item_segmentation");
+  pr2_pick_perception::ItemSegmentationService service("segment_items");
+  ros::spin();
+  return 0;
+}
