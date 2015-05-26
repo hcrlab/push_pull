09b71e53e4a34ce150017a74816d59943142aca7
Dan Butler <djbutler@cs.washington.edu>
2015-05-25 23:02:31 -0700

    moved missing depth pixel logic in verify grasp

--- pr2_pick_main/scripts/states/VerifyGrasp.py
+++ pr2_pick_main/scripts/states/VerifyGrasp.py
@@ -83,10 +83,6 @@ class VerifyGrasp(smach.State):
         box_request.dimensions.z = 0.2
         response = self._count_points_in_box(box_request)
 
-        img = np.array(cvb.imgmsg_to_cv(rospy.wait_for_message('/head_mount_kinect/depth_registered/image_raw',Image)))
-        num_missing_depth_pixels = np.sum(img[320:450,220:320]==0)
-        rospy.loginfo('Number of missing depth pixels near hand: {}'.format(num_missing_depth_pixels))
-
         box_pose = PoseStamped()
         box_pose.header.frame_id = 'torso_lift_link'
         box_pose.pose.position = box_request.center
@@ -97,7 +93,7 @@ class VerifyGrasp(smach.State):
             viz.publish_bounding_box(self._markers, box_pose, 0.12, 0.12, 0.2, 0.5,
                                      0.5, 0.5, 0.25, 2345)
             raw_input('[VerifyGrasp] Press enter to continue: ')
-        return response.num_points > 1400 or num_missing_depth_pixels > 1000
+        return response.num_points > 1400
 
     @handle_service_exceptions(outcomes.VERIFY_GRASP_FAILURE)
     def execute(self, userdata):
@@ -122,6 +118,13 @@ class VerifyGrasp(smach.State):
             self._optical_detect_item.wait_for_service()
             response = self._optical_detect_item(arm=OpticalRefineRequest.RIGHT_ARM)
             grasp_succeeded = grasp_succeeded or response.detect
+
+        # OR the depth pixels 
+        if not grasp_succeeded:
+            img = np.array(cvb.imgmsg_to_cv(rospy.wait_for_message('/head_mount_kinect/depth_registered/image_raw',Image)))
+            num_missing_depth_pixels = np.sum(img[320:450,220:320]==0)
+            rospy.loginfo('Number of missing depth pixels near hand: {}'.format(num_missing_depth_pixels))
+            grasp_succeeded = grasp_succeeded or num_missing_depth_pixels > 1000
 
         if grasp_succeeded:
             rospy.loginfo('[VerifyGrasp] Item in hand.')
