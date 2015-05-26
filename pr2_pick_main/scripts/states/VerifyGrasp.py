d21ff874ceca2c8af8c318052596dfab3698afbf
Dan Butler <djbutler@cs.washington.edu>
2015-05-25 23:48:25 -0700

    added timeout to verify grasp / missing depth

--- pr2_pick_main/scripts/states/VerifyGrasp.py
+++ pr2_pick_main/scripts/states/VerifyGrasp.py
@@ -121,7 +121,7 @@ class VerifyGrasp(smach.State):
 
         # OR the depth pixels 
         if not grasp_succeeded:
-            img = np.array(cvb.imgmsg_to_cv(rospy.wait_for_message('/head_mount_kinect/depth_registered/image_raw',Image)))
+            img = np.array(cvb.imgmsg_to_cv(rospy.wait_for_message('/head_mount_kinect/depth_registered/image_raw',Image,timeout=5)))
             num_missing_depth_pixels = np.sum(img[320:450,220:320]==0)
             rospy.loginfo('Number of missing depth pixels near hand: {}'.format(num_missing_depth_pixels))
             grasp_succeeded = grasp_succeeded or num_missing_depth_pixels > 1000
