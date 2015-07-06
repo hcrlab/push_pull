b5882b9713c0bcc76045f4db2c12d2ed02a4c7cf
Michelle Valente <ma3360@columbia.edu>
2015-07-06 13:18:01 -0700

    Adding new shelf

--- pr2_pick_main/scripts/states/FindShelf.py
+++ pr2_pick_main/scripts/states/FindShelf.py
@@ -47,63 +47,73 @@ class FindShelf(smach.State):
         with the shelf's pose in the odom_combined frame.
         '''
         rospy.sleep(10)
-        success = False
-        shelf_ps = PoseStamped()  # The shelf pose returned by the service.
-        shelf_odom = PoseStamped()  # Shelf pose in odom_combined frame.
-        for try_num in range(100):
-            self._localize_object.wait_for_service()
-            obj_request = ObjectDetectionRequest()
-            obj_request.obj_type = 'shelf'
-            obj_request.region2d = ROI2d(top_left_x=-1,
-                                         top_left_y=-1,
-                                         bottom_right_x=-1,
-                                         bottom_right_y=-1)
-            request = LocalizeShelfRequest()
-            request.object = obj_request
-            response = self._localize_object(request)
-            if len(response.locations.objects) == 0:
-                self._tts.publish('Shelf service returned no results.')
-                rospy.logwarn('[FindShelf]: Shelf service returned no results.')
-                continue
-            shelf = response.locations.objects[0]
-            rospy.loginfo('Returned shelf pose: {}'.format(shelf))
-            shelf_ps.pose = shelf.pose
-            shelf_ps.header = shelf.header
-            shelf_ps.header.stamp = rospy.Time(0)
+        # success = False
+        # shelf_ps = PoseStamped()  # The shelf pose returned by the service.
+        # shelf_odom = PoseStamped()  # Shelf pose in odom_combined frame.
+        # for try_num in range(100):
+        #     self._localize_object.wait_for_service()
+        #     obj_request = ObjectDetectionRequest()
+        #     obj_request.obj_type = 'shelf'
+        #     obj_request.region2d = ROI2d(top_left_x=-1,
+        #                                  top_left_y=-1,
+        #                                  bottom_right_x=-1,
+        #                                  bottom_right_y=-1)
+        #     request = LocalizeShelfRequest()
+        #     request.object = obj_request
+        #     response = self._localize_object(request)
+        #     if len(response.locations.objects) == 0:
+        #         self._tts.publish('Shelf service returned no results.')
+        #         rospy.logwarn('[FindShelf]: Shelf service returned no results.')
+        #         continue
+        #     shelf = response.locations.objects[0]
+        #     rospy.loginfo('Returned shelf pose: {}'.format(shelf))
+        #     shelf_ps.pose = shelf.pose
 
-            try:
-                shelf_odom = self._tf_listener.transformPose('odom_combined',
-                                                             shelf_ps)
-            except:
-                rospy.logerr(
-                    'No transform between {} and {} in FindShelf'.format(
-                        shelf.header.frame_id, 'odom_combined'))
-                self._tts.publish('No odometry transform while finding shelf.')
-                continue
+        #     shelf_ps.header = shelf.header
+        #     shelf_ps.header.stamp = rospy.Time(0)
 
-            roll, pitch, yaw = tf.transformations.euler_from_quaternion(
-                [shelf_odom.pose.orientation.x, shelf_odom.pose.orientation.y,
-                 shelf_odom.pose.orientation.z, shelf_odom.pose.orientation.w])
-            pitch_degs = 180 * pitch / math.pi
-            yaw_degs = 180 * yaw / math.pi
-            rospy.loginfo('roll: {}, pitch: {}, yaw: {}'.format(
-                180 * roll / math.pi, pitch_degs, yaw_degs))
+        #     try:
+        #         shelf_odom = self._tf_listener.transformPose('odom_combined',
+        #                                                      shelf_ps)
+        #     except:
+        #         rospy.logerr(
+        #             'No transform between {} and {} in FindShelf'.format(
+        #                 shelf.header.frame_id, 'odom_combined'))
+        #         self._tts.publish('No odometry transform while finding shelf.')
+        #         continue
 
-            # Check that the response is reasonable.
-            #if shelf_odom.pose.position.z < -0.30 or shelf_odom.pose.position.z > 0.30:
-            #    rospy.logwarn('[FindShelf]: Shelf not on the ground.')
-            #    self._tts.publish('Shelf not on the ground for try {}'.format(try_num))
-            #    continue
-            #if pitch_degs > 4 or pitch_degs < -4:
-            #    self._tts.publish('Shelf too tilted for try {}'.format(try_num))
-            #    rospy.logwarn('[FindShelf]: Shelf too tilted.')
-            #    continue
+        #     roll, pitch, yaw = tf.transformations.euler_from_quaternion(
+        #         [shelf_odom.pose.orientation.x, shelf_odom.pose.orientation.y,
+        #          shelf_odom.pose.orientation.z, shelf_odom.pose.orientation.w])
+        #     pitch_degs = 180 * pitch / math.pi
+        #     yaw_degs = 180 * yaw / math.pi
+        #     rospy.loginfo('roll: {}, pitch: {}, yaw: {}'.format(
+        #         180 * roll / math.pi, pitch_degs, yaw_degs))
 
-            success = True
-            break
+        #     # Check that the response is reasonable.
+        #     #if shelf_odom.pose.position.z < -0.30 or shelf_odom.pose.position.z > 0.30:
+        #     #    rospy.logwarn('[FindShelf]: Shelf not on the ground.')
+        #     #    self._tts.publish('Shelf not on the ground for try {}'.format(try_num))
+        #     #    continue
+        #     #if pitch_degs > 4 or pitch_degs < -4:
+        #     #    self._tts.publish('Shelf too tilted for try {}'.format(try_num))
+        #     #    rospy.logwarn('[FindShelf]: Shelf too tilted.')
+        #     #    continue
 
-        if not success:
-            return False, None
+        #     success = True
+        #     break
+
+        # if not success:
+        #     return False, None
+
+        shelf_odom = PoseStamped()
+        shelf_odom.header.frame_id = 'odom_combined'
+        shelf_odom.pose.position.x = 0.10
+        shelf_odom.pose.position.y = 0.0
+        shelf_odom.pose.position.z = 0.0
+        shelf_odom.pose.orientation.x = 0.0
+        shelf_odom.pose.orientation.y = 0.0
+        shelf_odom.pose.orientation.z = 0.0
         return success, shelf_odom
 
     @handle_service_exceptions(outcomes.FIND_SHELF_FAILURE)
